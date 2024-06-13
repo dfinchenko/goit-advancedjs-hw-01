3c3ea116b730ee040427fd4b2e5fba1cd4de9fb6
Denys Finchenko <dfinchenko@gmail.com>
2024-06-13 22:13:01 +0200

    Home Work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,14 +6,14 @@ const player = new Player(iframe);
 const LS_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 const onPlay = function ({ seconds }) {
-  localStorage.setItem(LS_CURRENT_TIME_KEY, seconds);
+  if (seconds !== undefined) {
+    localStorage.setItem(LS_CURRENT_TIME_KEY, seconds);
+  }
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 const currentTime = localStorage.getItem(LS_CURRENT_TIME_KEY);
 if (currentTime !== null) {
-  player.setCurrentTime(+currentTime).catch(function (error) {
-    console.error('Error setting current time:', error);
-  });
+  player.setCurrentTime(+currentTime);
 }
