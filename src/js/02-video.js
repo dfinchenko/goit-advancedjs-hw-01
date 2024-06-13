ed4a500ed070729f6f0048ce9aa417c91ba88957
Denys Finchenko <dfinchenko@gmail.com>
2024-06-13 22:36:18 +0200

    Home Work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const LS_CURRENT_TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LS_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 // const onPlay = function ({ seconds }) {
 //   if (seconds !== undefined) {
