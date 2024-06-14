a2d38bbdb2ef01a027f6adea1aca5701a9c3d3b1
Denys Finchenko <dfinchenko@gmail.com>
2024-06-14 09:20:48 +0200

    Home Work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,19 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const LS_CURRENT_TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LS_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-// const onPlay = function ({ seconds }) {
-//   if (seconds !== undefined) {
-//     localStorage.setItem(LS_CURRENT_TIME_KEY, seconds);
-//   }
-// };
+const onPlay = function ({ seconds }) {
+  if (seconds !== undefined) {
+    localStorage.setItem(LS_CURRENT_TIME_KEY, seconds);
+  }
+};
 
-// player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
-// const currentTime = localStorage.getItem(LS_CURRENT_TIME_KEY);
-// if (currentTime !== null) {
-//   player.setCurrentTime(+currentTime);
-// }
+const currentTime = localStorage.getItem(LS_CURRENT_TIME_KEY);
+if (currentTime !== null) {
+  player.setCurrentTime(+currentTime);
+}
